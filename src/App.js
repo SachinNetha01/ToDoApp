31f5963f1bfb0092240e277bcc115c9b4007e905
Sachin Netha <sachinnetha01@gmail.com>
2022-05-11 22:00:16 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -28,12 +28,12 @@ function App() {
 <div className='card'>
   <div className='card-body'>
     <div className='card-title'>
-    <h6>To do Management App!</h6><br/>
+    <h6>To-do Management App!</h6><br/>
     <form onSubmit={submitHandler}>
       <input type="text" size="30" value={task} name='task' onChange={changeHandler}/> &nbsp;
       <input type="Submit" value="Add" name='Add'/>
     </form>&nbsp;
-    <TodosList props={todos} deleteHandler={deleteHandler} />
+    <TodosList value={todos} deleteHandler={deleteHandler} />
     </div>
   </div>
 </div>
--- src/TodosList.js
+++ src/TodosList.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const TodosList = ({props,deleteHandler}) => {
+const TodosList = ({value,deleteHandler}) => {
   return (
     <div>{
-        props.map((todo,index)=><div key={index}>
+        value.map((todo,index)=><div key={index}>
             <h5>{todo} &nbsp; <button onClick={()=>{deleteHandler(index)}} size="10"> Remove</button></h5>
         </div>)}
         </div>
